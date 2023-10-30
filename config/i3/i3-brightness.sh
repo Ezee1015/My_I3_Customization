1a31933e18e7e71cd3ca4c04ced93d4ec38ae690
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-10-30 20:07:19 -0300

    Improve the brightness script

--- config/i3/i3-brightness.sh
+++ config/i3/i3-brightness.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
-#
-OUTPUT=$(xrandr --current | grep " connected" | awk '{print $1}')
 
-CURRENT_BRIGHTNESS=$(xrandr --prop --verbose | grep -A10 " connected" | grep "Brightness" | awk '{print $2}')
+if [ -z "$OUTPUT" ]; then
+  OUTPUT=$(xrandr --current | grep " connected" | awk '{print $1}')
+fi
 
 if [ -z "$JUMP" ]; then
   JUMP="0.2"
 fi
+
+CURRENT_BRIGHTNESS=$(xrandr --prop --verbose | grep -A10 " connected" | grep "Brightness" | awk '{print $2}')
 
 if [ "$1" = "-inc" ]; then
   xrandr --output $OUTPUT --brightness $(echo "$CURRENT_BRIGHTNESS $JUMP" | awk '{print $1+$2}')
@@ -17,11 +19,15 @@ elif [ "$1" = "-reset" ]; then
 elif [ "$1" = "-set" ] & [ ! -z "$2" ]; then
   xrandr --output $OUTPUT --brightness $2
 else
-  echo "$0 Help manual...\n"
-  echo "$0 -inc                  Increase brightness"
-  echo "$0 -dec                  Decrease brightness"
-  echo "$0 -reset                Reset the brightness to the original value (1.0)"
-  echo "$0 -set [value]          Set the brightness to a given value"
+  echo "HELP MANUAL\n"
+  echo "- Variables..."
+  echo "OUTPUT=[output] $0 [opts]          Specify the output"
+  echo "JUMP=[jump] $0 [opts]              Specify the the increase/decrease value"
+  echo "\n- Options..."
+  echo "$0 -inc                            Increase brightness"
+  echo "$0 -dec                            Decrease brightness"
+  echo "$0 -reset                          Reset the brightness to the original value (1.0)"
+  echo "$0 -set [value]                    Set the brightness to a given value"
   exit 1
 fi
 
