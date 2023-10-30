9662570c4bdd9f74145b44c45e77fe5098ecf6f5
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-10-30 19:24:10 -0300

    Add a basic brightness control

---
+++ config/i3/i3-brightness.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+#
+OUTPUT=$(xrandr --current | grep " connected" | awk '{print $1}')
+
+CURRENT_BRIGHTNESS=$(xrandr --prop --verbose | grep -A10 " connected" | grep "Brightness" | awk '{print $2}')
+
+if [ -z "$JUMP" ]; then
+  JUMP="0.2"
+fi
+
+if [ "$1" = "-inc" ]; then
+  xrandr --output $OUTPUT --brightness $(echo "$CURRENT_BRIGHTNESS $JUMP" | awk '{print $1+$2}')
+elif [ "$1" = "-dec" ]; then
+  xrandr --output $OUTPUT --brightness $(echo "$CURRENT_BRIGHTNESS $JUMP" | awk '{print $1-$2}')
+elif [ "$1" = "-reset" ]; then
+  xrandr --output $OUTPUT --brightness 1.0
+elif [ "$1" = "-set" ] & [ ! -z "$2" ]; then
+  xrandr --output $OUTPUT --brightness $2
+else
+  echo "$0 Help manual...\n"
+  echo "$0 -inc                  Increase brightness"
+  echo "$0 -dec                  Decrease brightness"
+  echo "$0 -reset                Reset the brightness to the original value (1.0)"
+  echo "$0 -set [value]          Set the brightness to a given value"
+  exit 1
+fi
+
+
+CURRENT_BRIGHTNESS=$(xrandr --prop --verbose | grep -A10 " connected" | grep "Brightness" | awk '{print $2}')
+BRIGHTNESS_PERCENTAGE=$(echo "$CURRENT_BRIGHTNESS 100" | awk '{print $1*$2}')
+dunstify -a "Script de brillo" -u low -r 9993 -h int:value:"$BRIGHTNESS_PERCENTAGE" -i "redshift-status-day" "Brillo: " -t 2000
