1ddb679f2e8cf1b642fd535eb969428e1fcf09f0
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-11-01 06:53:04 -0300

    Transfer the weather script to a personal repo

--- config/i3/myScripts/weather.sh
+++
@@ -1,5 +0,0 @@
-curl -Ss 'https://wttr.in?0&T&Q' | cut -c 16- | head -2 | xargs echo
-
-case $BLOCK_BUTTON in
-  1) xdg-open "https://www.accuweather.com/es/ar/saavedra/1228300/weather-forecast/1228300";; # left click
-esac
