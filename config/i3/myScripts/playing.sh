5603d51e57e5f8c541fcc7748ed51a2ebc058bb9
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2023-11-04 20:51:41 -0300

    Comment the parts of the script

--- config/i3/myScripts/playing.sh
+++ config/i3/myScripts/playing.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 music_file=""
 file_count=0
 pids=$(xdotool search --name "MPV - ")
@@ -7,18 +8,22 @@ for pid in $pids; do
   music_file=$(xdotool getwindowname $pid | cut -c 7-)
 done
 
+# Indicate if there's more than one file playing
 if [ $file_count -gt 1 ]; then
   music_file="[$file_count] $music_file"
 fi
 
+# Cut music file name if it's too long
 if [ ${#music_file} -gt 63 ]; then
   music_file="${music_file:0:60}..."
 fi
 
+# Put icon at the beginning if music is playing
 if [ ! -z "$music_file" ]; then
   echo "♪ $music_file"
 fi
 
+# Mouse actions
 case $BLOCK_BUTTON in
   1) # left click  - Pause music only if there's one mpv instance
     if [ $file_count -eq 1 ]; then
