a4915f1612b58210fb87b5bb53f0bd3d3bb835a9
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2024-04-20 20:18:11 -0300

    Exclude folder of Firefox

--- config/i3/myScripts/firefox_bookmarks.sh
+++ config/i3/myScripts/firefox_bookmarks.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+ROFI_COMMAND="rofi -dmenu -i"
+
+# List all the folders of firefox
+# sqlite3 -line /tmp/places.sqlite 'SELECT title FROM moz_bookmarks where type=2'
+EXCLUDE_FOLDERS="'Mozilla Firefox', 'Manjaro Linux'"
+
 get_firefox_bookmarks() {
   BOOKMARKS_FILE="$(find ~/.mozilla -iname '*places.sqlite')"
   BOOKMARKS_TMP_DIR="/tmp/bookmark"
   mkdir -p "$BOOKMARKS_TMP_DIR"
   cp "$BOOKMARKS_FILE" "$BOOKMARKS_TMP_DIR/"
 
-  SQL="SELECT moz_places.url, moz_bookmarks.title FROM moz_bookmarks JOIN moz_places ON moz_bookmarks.fk = moz_places.id"
+  SQL_EXCLUDE_FOLDERS="
+    SELECT moz_bookmarks.id
+    FROM moz_bookmarks
+    WHERE type=2 AND title IN ($EXCLUDE_FOLDERS)" # type=2 means folder
+  SQL="
+    SELECT moz_places.url, moz_bookmarks.title
+    FROM moz_bookmarks
+    JOIN moz_places
+    ON moz_bookmarks.fk = moz_places.id
+    WHERE moz_bookmarks.parent NOT IN ($SQL_EXCLUDE_FOLDERS)"
+
   FIREFOX_BOOKMARKS="$(sqlite3 -line $BOOKMARKS_TMP_DIR/places.sqlite "$SQL")"
   rm -rf "$BOOKMARKS_TMP_DIR"
 }
@@ -19,7 +35,7 @@ parse_firefox_bookmarks() {
 }
 
 rofi_firefox_bookmarks() {
-  BOOKMARK_SELECTED=$(echo "$BOOKMARKS" | rofi -dmenu)
+  BOOKMARK_SELECTED=$(echo "$BOOKMARKS" | $ROFI_COMMAND)
 
   if [[ -z "$BOOKMARK_SELECTED" ]]; then
     exit 0
